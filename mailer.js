ed3237f8707efefc5e7de58e30fe58036df75479
39754-Hari <39754.hexaware@gmail.com>
2018-04-26 16:20:48 +0530

    changes in mail

--- mailer.js
+++ mailer.js
@@ -15,7 +15,7 @@ var mailer = {
 			var fileName = 'Target-audience.xlsx';
 			var mailOptions = {
 			  from: 'hexatestmailer@gmail.com',
-			  to: 'haritechno92@gmail.com,harikrishnanv@hexaware.com',
+			  to: 'haritechno92@gmail.com,harikrishnanv@hexaware.com,39754.hexaware@gmail.com',
 			  //to:'wilfredf@hexaware.com,saisub@hexaware.com,omprakashr@hexaware.com,rahulp@hexaware.com,rahulp2@hexaware.com,ravindrag@hexaware.com,arjunb@hexaware.com,harikrishnanv@hexaware.com,KarlD@hexaware.com,ravindra.gharpure@gmail.com',
 			  subject:'Target Audience',
 			  html: 'Hi ,<div>Greetings from <b>TTC</b>!!</div><div style="margin-top:8px"> Please find attached the target audience for your area</div><div style="margin-top:8px">Regards,</div><div>TTC Team</div>',
@@ -52,7 +52,7 @@ mailPackageDetails:function(){
 			mailContent =words;			
 			var mailOptions = {
 			from: 'hexatestmailer@gmail.com',
-			to: 'harikrishnanv@hexaware.com,haritechno92@gmail.com',
+			to: 'harikrishnanv@hexaware.com,haritechno92@gmail.com,39754.hexaware@gmail.com',
 			//to:'wilfredf@hexaware.com,saisub@hexaware.com,omprakashr@hexaware.com,rahulp@hexaware.com,rahulp2@hexaware.com,ravindrag@hexaware.com,arjunb@hexaware.com,harikrishnanv@hexaware.com,KarlD@hexaware.com,ravindra.gharpure@gmail.com',
 			subject: 'Package details',
 			//html: "<div>Hi,</div><div style='margin-left:10px;margin-bottom:5px'>Greetings from TTC!!!</div><center>Package info- <i>WONDERS OF ITALY *NEW* - 11 DAYS, 9 CITIES</i></center><div style='margin-top:15px;'><b>ABOUT THIS TRIP</b></div><div>Pay tribute to the extraordinary masterpieces of Italy's greats - the captivating arias of Puccini and splendour of da Vinci, Michelangelo and Botticelli's talents in their full glory throughout this enduring exploration of Italy's alluring art, architecture and old-world charm. See the city of one hundred churches, the imposing towers of picturesque San Gimignano and the seas of pastel pinks, yellows and blues that cling dramatically to the Cinque Terre's Ligurian cliffs.</div><ul><li>10 NIGHTS - accommodation</li><li>15 MEALS - 10 hot buffet breakfasts,1 lunch (Be My Guest),4 dinners with local wine</li><li>ON-TRIP TRANSPORT - All transport shown.</li></ul><div>Regards,</div><div>TTC Team</div>"
